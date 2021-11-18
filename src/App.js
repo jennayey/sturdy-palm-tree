8b0f5e6668372e19eaa931bb423a86c16d3f42ad
Jenna Angala <jennayey@gmail.com>
2021-11-18 23:19:21 +0800

    style: updated colors

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ function App(props) {
 
   const drawer = (
     <div>
-      <Toolbar />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+      <Toolbar/>
+      <List sx={{marginTop: `20px`}}>
+        {["Home", "Enrollment", "Account Policy", "Benefit Summary", "Enterprise Summary", "Reports", "Maintenance"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -42,7 +42,7 @@ function App(props) {
       </List>
       <Divider />
       <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
+        {["User Profile", "Quick Guide", "Log Out"].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -51,6 +51,8 @@ function App(props) {
           </ListItem>
         ))}
       </List>
+      <Divider />
+ 
     </div>
   );
 
@@ -58,7 +60,7 @@ function App(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", backgroundColor: "#efefef",}}>
       <CssBaseline />
       <AppBar
         position="fixed"
@@ -67,12 +69,13 @@ function App(props) {
 
           ml: { md: `${drawerWidth}px` },
           zIndex: 2000,
+          backgroundColor: "white"
           
         }}
       >
         <Toolbar sx={{display: "flex", justifyContent: "space-between"}} >
           <IconButton
-            color="inherit"
+            color="primary"
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
@@ -80,10 +83,10 @@ function App(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" noWrap component="div">
+          <Typography color="primary" variant="h6" noWrap component="div">
             Cocolife MyPolicy
           </Typography>
-          <IconButton color="inherit" aria-label="profile" href="">
+          <IconButton color="primary" aria-label="profile" href="">
             <AccountCircle />
           </IconButton>
         </Toolbar>
@@ -129,41 +132,15 @@ function App(props) {
       <Box
         component="main"
         sx={{
+         height:"100%",
           flexGrow: 1,
           p: 3,
           width: { sm: `calc(100% - ${drawerWidth}px)` },
         }}
       >
         <Toolbar />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        <Typography variant="h5" noWrap component="div">
+Benefit Summary          </Typography>
       </Box>
     </Box>
   );
