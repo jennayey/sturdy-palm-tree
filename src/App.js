53289a343f469bfe232947443ee579b6a06f036a
Jenna Angala <jennayey@gmail.com>
2021-11-23 12:12:35 +0800

    test: theming

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
-import { AccountCircle } from '@mui/icons-material';
+import { AccountCircle } from "@mui/icons-material";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
@@ -16,9 +16,13 @@ import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
+import customtheme from "./customtheme"
 
 const drawerWidth = 240;
 
+let cTheme = createTheme(customtheme)
 function App(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
@@ -29,9 +33,17 @@ function App(props) {
 
   const drawer = (
     <div>
-      <Toolbar/>
-      <List sx={{marginTop: `20px`}}>
-        {["Home", "Enrollment", "Account Policy", "Benefit Summary", "Enterprise Summary", "Reports", "Maintenance"].map((text, index) => (
+      <Toolbar />
+      <List sx={{ marginTop: `20px` }}>
+        {[
+          "Home",
+          "Enrollment",
+          "Account Policy",
+          "Benefit Summary",
+          "Enterprise Summary",
+          "Reports",
+          "Maintenance",
+        ].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>
               {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
@@ -52,7 +64,6 @@ function App(props) {
         ))}
       </List>
       <Divider />
- 
     </div>
   );
 
@@ -60,89 +71,91 @@ function App(props) {
     window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: "flex", backgroundColor: "#efefef",}}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: `100%`,
+    <ThemeProvider theme={cTheme}>
+      <Box sx={{ display: "flex", backgroundColor: "#efefef" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: `100%`,
 
-          ml: { md: `${drawerWidth}px` },
-          zIndex: 2000,
-          backgroundColor: "white"
-          
-        }}
-      >
-        <Toolbar sx={{display: "flex", justifyContent: "space-between"}} >
-          <IconButton
-            color="primary"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { md: "none" } }}
+            ml: { md: `${drawerWidth}px` },
+            zIndex: 2000,
+            backgroundColor: "white",
+          }}
+        >
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <IconButton
+              color="primary"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { md: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography color="primary" variant="h6" noWrap component="div">
+              Cocolife MyPolicy
+            </Typography>
+            <IconButton color="primary" aria-label="profile" href="">
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography color="primary" variant="h6" noWrap component="div">
-            Cocolife MyPolicy
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", md: "block" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          component="main"
+          sx={{
+            height: "100%",
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+          }}
+        >
+          <Toolbar />
+          <Typography variant="h5" noWrap component="div">
+            Benefit Summary{" "}
           </Typography>
-          <IconButton color="primary" aria-label="profile" href="">
-            <AccountCircle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", md: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
+        </Box>
       </Box>
-      <Box
-        component="main"
-        sx={{
-         height:"100%",
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Typography variant="h5" noWrap component="div">
-Benefit Summary          </Typography>
-      </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 export default App;
---
+++ src/customtheme.js
@@ -0,0 +1,38 @@
+
+
+ const customtheme = {
+      palette: {
+    type: 'light',
+    primary: {
+      main: '#cf32fa',
+    },
+    secondary: {
+      main: '#f5209b',
+    },
+    text: {
+      primary: 'rgba(43,39,39,0.91)',
+    },
+    background: {
+      default: '#f3f3f3',
+    },
+  },
+  typography: {
+    fontFamily: 'Libre Franklin',
+    h1: {
+      fontWeight: 600,
+      fontFamily: 'Roboto',
+      fontSize: '6rem',
+    },
+    h2: {
+      fontWeight: 600,
+    },
+    h5: {
+      fontWeight: 600,
+      fontSize: '1.1rem',
+      fontFamily: 'Roboto',
+    },
+    fontWeightBold: 700,
+  },
+};
+
+export default customtheme;
