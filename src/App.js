74c369245d98fd6c6c65905603812be9d621a9ff
Jenna Angala <jennayey@gmail.com>
2021-12-28 00:00:10 +0800

    feat: initial implementation of dark mode

--- src/App.js
+++ src/App.js
@@ -1,71 +1,125 @@
- import React from 'react'
- import Login from "./Login";
+import React from "react";
+import Login from "./Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Box from "@mui/material/Box";
 
-import { ThemeProvider } from "@mui/material/styles";
-import { createTheme } from "@mui/material/styles";
+// import { ThemeProvider } from "@mui/material/styles";
+import { useTheme, createTheme, ThemeProvider } from "@mui/material/styles";
 import customtheme from "./theme/customtheme";
 
-import MyPolicyHome from "./MyPolicy/MyPolicyHome"
+import MyPolicyHome from "./MyPolicy/MyPolicyHome";
 
-import ForgotPassword from "./ForgotPassword"
-import ForgotPassword2 from "./ForgotPassword2"
-import UserRegistration from "./MyPolicy/UserRegistration" 
-import UserRegistration2 from "./MyPolicy/UserRegistration2" 
+import ForgotPassword from "./ForgotPassword";
+import ForgotPassword2 from "./ForgotPassword2";
+import UserRegistration from "./MyPolicy/UserRegistration";
+import UserRegistration2 from "./MyPolicy/UserRegistration2";
 import GLOESHome from "./GLOES/GLOESHome";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Switch from "@mui/material/Switch";
+import IconButton from "@mui/material/IconButton";
 
- export default function App() {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+function Test() {
+  const theme = useTheme();
+  
+  return (
+
+    <Box
+    sx={{
+      display: 'flex',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      bgcolor: 'background.default',
+      color: 'text.primary',
+      borderRadius: 1,
+      p: 3,
+    }}>
+       {theme.palette.mode} mode
+      {/* <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        <Brightness7Icon />
+      </IconButton> */}
+      <Login/>
+    </Box>
+  );
+}
+export default function App() {
+  const coloRMode = React.useContext(ColorModeContext);
   let cTheme = createTheme(customtheme);
+  const [mode, setMode] = React.useState('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette:{
+          mode,
+              ...(mode === "light"
+                ? {
+                    primary: {
+                      main: "#005db9",
+                    },
+                    secondary: {
+                      main: "#00af43",
+                    },
+                    text: {
+                      primary: "rgba(43,39,39,0.91)",
+                      hint: "rgba(0,0,0,0.38)",
+                    },
+                    background: {
+                      // default: '#eff8fa',
+                      // default: "rgba(248,250,253,1)",
+                      default: "white",
+                    },
+                  }
+                : {
+                    primary: {
+                      main: "#fff",
+                    },
+                    secondary: {
+                      main: "rgba(255, 255, 255, 0.7)",
+                    },
+                    text: {
+                      primary: "#fff",
+                      hint: "rgba(0,0,0,0.38)",
+                    },
+                    background: {
+                      // default: '#eff8fa',
+                      // default: "rgba(248,250,253,1)",
+                      default: "#121212",
+                    },
+                  }),
+        },
+       
+      }),
+    [mode]
+  );
 
-  const getDesignTokens = (mode) => ({
-    palette: {
-      mode,
-      ...(mode === 'light'
-        ? {
-          primary: {
-            main: "#005db9",
-          },
-          secondary: {
-            main: "#00af43",
-          },
-          text: {
-            primary: "rgba(43,39,39,0.91)",
-            hint: "rgba(0,0,0,0.38)",
-          },
-          background: {
-            // default: '#eff8fa',
-            // default: "rgba(248,250,253,1)",
-            default: "white"
-          },
-          }
-        : {
-          primary: {
-            main: "#fff",
-          },
-          secondary: {
-            main: "rgba(255, 255, 255, 0.7)",
-          },
-          text: {
-            primary: "#fff",
-            hint: "rgba(0,0,0,0.38)",
-          },
-          background: {
-            // default: '#eff8fa',
-            // default: "rgba(248,250,253,1)",
-            default: "#121212"
   
-          },
-          }),
-    },
-  });
-  
-  // const theme = useTheme();
-  // const colorMode = React.useContext(ColorModeContext);
-  
-   return (
+
+  return (
     <BrowserRouter>
-    <ThemeProvider theme={cTheme}>
-      <Routes>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+        <IconButton
+        sx={{ ml: 1 }}
+        onClick={colorMode.toggleColorMode}
+        color="inherit"
+      >
+        <Brightness7Icon />
+      </IconButton>
+        <Routes>
         <Route path="/" element={<Login />}/>
         <Route path="gloes" element={<GLOESHome />}/>
         <Route path="mypolicy" element={<MyPolicyHome />}/>
@@ -74,8 +128,8 @@ import GLOESHome from "./GLOES/GLOESHome";
         <Route path="registration" element={<UserRegistration />}/>
         <Route path="registration-step2" element={<UserRegistration2 />}/>
       </Routes>
-    </ThemeProvider>
-  </BrowserRouter>
-   )
- }
- 
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </BrowserRouter>
+  );
+}
